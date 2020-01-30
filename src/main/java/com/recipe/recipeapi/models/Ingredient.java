0d530882d98b1e64de50c6b402c65b0083a34da2
Lucas Alves <lcluphs@gmail.com>
2020-01-30 11:48:25 -0300

    created ingredient entity

--- src/main/java/com/recipe/recipeapi/models/Ingredient.java
+++ src/main/java/com/recipe/recipeapi/models/Ingredient.java
@@ -9,20 +9,14 @@ import javax.persistence.Table;
 import lombok.Builder;
 import lombok.Data;
 
+@Data
 @Entity
 @Builder
-@Data
-@Table(name="TB_USER")
-public class User extends Abstract implements Serializable{
-	
+@Table(name = "TB_INGREDIENT")
+public class Ingredient extends Abstract implements Serializable{
+
 	private static final long serialVersionUID = -7169638148702718527L;
 
 	@Column(nullable = false)
 	private String nome;
-	
-	@Column(nullable = false)
-	private String login;
-	
-	@Column(nullable = false)
-	private String password;
 }
