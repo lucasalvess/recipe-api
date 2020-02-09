7f3e2c215187251c11e921475ba5fe137e0c6bcf
Lucas Alves <lcluphs@gmail.com>
2020-02-09 13:00:57 -0300

    documented resources of user

---
+++ src/main/java/com/recipe/recipeapi/resource/documentation/UserResourceDocumentation.java
@@ -0,0 +1,33 @@
+package com.recipe.recipeapi.resource.documentation;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.recipe.recipeapi.models.User;
+
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
+@ApiResponses(value = { @ApiResponse(code = 401, message = "Unauthorized"),
+		@ApiResponse(code = 403, message = "Forbidden"), @ApiResponse(code = 404, message = "Not Found") })
+public interface UserResourceDocumentation {
+	
+	@ApiOperation(value = "Sign in with you login and password")
+	@ApiResponse(code = 200, message = "Ok", response = User.class)
+	public User login(@PathVariable String login,@PathVariable String password);
+	
+	@ApiOperation(value = "List all users")
+	@ApiResponse(code = 200, message = "Ok", response = User.class)
+	public List<User> list();
+	
+	@ApiOperation(value = "Create a new user")
+	@ApiResponse(code = 201, message = "Ok", response = User.class)
+	public User save(@RequestBody User user);
+	
+	@ApiOperation(value = "Update an user (you can also create here)")
+	@ApiResponse(code = 200, message = "Ok", response = User.class)
+	public User update(@RequestBody User user);
+}
