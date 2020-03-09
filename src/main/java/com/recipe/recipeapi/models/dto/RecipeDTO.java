0419ff82dd965ef53bd8c8471ce1626f319cab5e
Lucas Alves <lcluphs@gmail.com>
2020-03-08 22:43:02 -0300

    create recipe dto

--- src/main/java/com/recipe/recipeapi/models/dto/RecipeDTO.java
+++ src/main/java/com/recipe/recipeapi/models/dto/RecipeDTO.java
@@ -1,44 +1,34 @@
-package com.recipe.recipeapi.models;
+package com.recipe.recipeapi.models.dto;
 
 import java.io.Serializable;
 import java.util.List;
-import java.util.Locale.Category;
 import java.util.UUID;
+import java.util.Locale.Category;
 
 import javax.persistence.Column;
-import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
-import javax.persistence.Table;
-import javax.validation.constraints.Positive;
 
-import org.hibernate.annotations.Type;
-import org.hibernate.annotations.Where;
+import com.recipe.recipeapi.models.Ingredient;
+import com.recipe.recipeapi.models.Recipe;
+import com.recipe.recipeapi.models.User;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
-@Entity
 @Data
 @Builder
-@AllArgsConstructor
 @NoArgsConstructor
-@Table(name="TB_RECIPE")
-@Where(clause = "deleted = 0")
-public class Recipe extends AbstractModel implements Serializable{
+@AllArgsConstructor
+public class RecipeDTO implements Serializable{
 	
 	private static final long serialVersionUID = -7169638148702718527L;
 	
-	@Builder.Default
-	@EqualsAndHashCode.Include
-	@Type(type = "uuid-char")
-	@Column(nullable = false)
-	private UUID uuid = UUID.randomUUID();
+	private UUID uuid;
 	
 	@Column(nullable = false)
 	private String name;
@@ -50,13 +40,23 @@ public class Recipe extends AbstractModel implements Serializable{
 
 	private String stepBystep;
 	
-	@Positive
 	private int time;
 	
 	@OneToMany
 	private List<Ingredient> ingredients;
 	
 	@OneToOne
-	private User chef;	
+	private User chef;
+	
+	public RecipeDTO(Recipe recipe) {
+		this.uuid = recipe.getUuid();
+		this.name = recipe.getName();
+		this.description = recipe.getDescription();
+		this.category = recipe.getCategory();
+		this.stepBystep = recipe.getStepBystep();
+		this.time = recipe.getTime();
+		this.ingredients = recipe.getIngredients();
+		this.chef = recipe.getChef();
+	}
 
 }
