3afdd66ede9cb1d76cf6381e53824146c3273039
Lucas Alves <lcluphs@gmail.com>
2020-03-20 12:07:20 -0300

    create find by id search in recipe repository

--- src/main/java/com/recipe/recipeapi/repository/RecipeRepository.java
+++ src/main/java/com/recipe/recipeapi/repository/RecipeRepository.java
@@ -1,6 +1,7 @@
 package com.recipe.recipeapi.repository;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -13,4 +14,6 @@ public interface RecipeRepository extends JpaRepository<Recipe, Long> {
 	
 	@Query("SELECT r.id, r.name, r.category, r.description FROM Recipe as r WHERE r.chef.id = ?1")
 	public List<Recipe> findByUser(Long id);
+	
+	public Recipe findByUuid(UUID uuid);
 }
