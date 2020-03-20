9486f01f869e9cdb0333b0d265c256fbda338d7e
Lucas Alves <lcluphs@gmail.com>
2020-03-20 12:56:36 -0300

    create recipe resources

--- src/main/java/com/recipe/recipeapi/resource/RecipeResource.java
+++ src/main/java/com/recipe/recipeapi/resource/RecipeResource.java
@@ -1,14 +1,24 @@
 package com.recipe.recipeapi.resource;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.recipe.recipeapi.models.dto.RecipeDTO;
+import com.recipe.recipeapi.models.form.RecipeForm;
+import com.recipe.recipeapi.models.form.RecipeFormPut;
 import com.recipe.recipeapi.resource.documentation.RecipeResourceDocumentation;
 import com.recipe.recipeapi.service.RecipeService;
 
@@ -20,7 +30,28 @@ public class RecipeResource implements RecipeResourceDocumentation{
 	private RecipeService service;
 	
 	@GetMapping
-	public List<RecipeDTO> list(){
-		return service.findAllRecipes();
+	public List<RecipeDTO> listByChef(Long id){
+		return service.findRecipesByChef(id);
+	}
+	
+	@GetMapping(path = "/{uuid}")
+	public RecipeDTO findByUuid(@PathVariable UUID uuid) {
+		return service.findRecipeByUuid(uuid);
+	}
+	
+	@PostMapping
+	public RecipeDTO create(@RequestBody RecipeForm recipeForm) {
+		return service.createRecipe(recipeForm);
+	}
+	
+	@PutMapping
+	public RecipeDTO update(@RequestBody RecipeFormPut recipeFormPut) {
+		return service.updateRecipe(recipeFormPut);
+	}
+	
+	@DeleteMapping(path = "/{uuid}")
+	@ResponseStatus(value = HttpStatus.NO_CONTENT)
+	public void delete(@PathVariable UUID uuid) {
+		service.deleteRecipeByUuid(uuid);
 	}
 }
--- src/main/java/com/recipe/recipeapi/resource/documentation/RecipeResourceDocumentation.java
+++ src/main/java/com/recipe/recipeapi/resource/documentation/RecipeResourceDocumentation.java
@@ -1,7 +1,41 @@
 package com.recipe.recipeapi.resource.documentation;
 
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.recipe.recipeapi.models.dto.RecipeDTO;
+import com.recipe.recipeapi.models.form.RecipeForm;
+import com.recipe.recipeapi.models.form.RecipeFormPut;
+
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
+@ApiResponses(value = { @ApiResponse(code = 401, message = "Unauthorized"),
+		@ApiResponse(code = 403, message = "Forbidden"), @ApiResponse(code = 404, message = "Not Found") })
 public interface RecipeResourceDocumentation {
+
+	@ApiOperation(value = "List all recipes by chef")
+	@ApiResponse(code = 200, message = "Ok", response = RecipeDTO.class)
+	public List<RecipeDTO> listByChef(Long id);
 	
+	@ApiOperation(value = "Find recipe by uuid")
+	@ApiResponse(code = 200, message = "Ok", response = RecipeDTO.class)
+	public RecipeDTO findByUuid(@PathVariable UUID uuid);
 	
+	@ApiOperation(value = "Create a new recipe")
+	@ApiResponse(code = 201, message = "Ok", response = RecipeDTO.class)
+	public RecipeDTO create(@RequestBody RecipeForm recipeForm);
+	
+	@ApiOperation(value = "Update a recipe")
+	@ApiResponse(code = 200, message = "Ok", response = RecipeDTO.class)
+	public RecipeDTO update(@RequestBody RecipeFormPut recipeFormPut);
+	
+	@ApiOperation(value = "Delete a recipe")
+	@ApiResponse(code = 204, message = "Ok", response = RecipeDTO.class)
+	public void delete(@PathVariable UUID uuid);
 
 }
